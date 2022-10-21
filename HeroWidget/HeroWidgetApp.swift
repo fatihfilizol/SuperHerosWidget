fb16d650a7e6a8eb3d6f4f6ec9b0022e97fccbd7
fatihfilizol <fatihfilizol@gmail.com>
2022-10-21 09:15:38 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Fatih Filizol on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
